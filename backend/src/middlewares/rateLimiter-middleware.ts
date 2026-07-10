db5c8d6d6777d228fca409dcb79cc6c8cc473a68
Hamza <hamzafareed616@gmail.com>
2026-07-11 03:36:38 +0500

    fix: rate limiting

--- backend/src/middlewares/rateLimiter-middleware.ts
+++ backend/src/middlewares/rateLimiter-middleware.ts
@@ -3,7 +3,7 @@ import { ERROR_MESSAGE } from "../constants/error-message";
 
 
 export const Limiter = rateLimit({
-    windowMs:5*60*1000,
-    max:100,
+    windowMs:1*60*1000,
+    max:200,
     message:ERROR_MESSAGE.TOO_MANY_REQUEST
 })
