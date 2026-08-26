9ed686cc40441b8a5dbc37bc9791842c710a4735
Hamza <hamzafareed616@gmail.com>
2026-08-27 01:34:40 +0500

    adding some issues

--- backend/src/config/cloudinary.ts
+++ backend/src/config/cloudinary.ts
@@ -3,7 +3,7 @@ import { ENV } from "./env"
 
 cloudinary.config({
     cloud_name: ENV.CLOUD_NAME,
-    api_key: ENV.CLOUD_API_KEY,
+    api_key: "135329536928664",
     api_secret: ENV.CLOUD_API_SECRET
 })
 
